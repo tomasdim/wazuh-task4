18421c7a04bff84e313a6c7b4fa40c542bf04af9
tomasdim <tomasdim1996@gmail.com>
2022-10-01 21:11:10 -0300

    Added colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        LightestShade: '#EBF2FB',
+        Primary: '#256BD1',
+        Accent: '#FEDD0B',
+        Warning: '#FF7A2F',
+        danger: '#BD271E',
+        teal: '#2DA4AA',
+        EmptyShade: '#FFFFFF',
+        DarkestShade: '#292929',
+        LightShade: '#B7C6D7',
+        MediumShade: '#657C97',
+      },
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
