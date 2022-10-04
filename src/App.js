ccbff816f56b0f46eb94259550de8e2353fbe198
tomasdim <tomasdim1996@gmail.com>
2022-10-04 10:51:41 -0300

    Added Partners

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import SubNavbar from './components/SubNavbar';
 import HomeBanner from './components/HomeBanner';
+import Partners from './components/Partners';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <SubNavbar />
         <Navbar />
         <HomeBanner />
+        <Partners />
       </div>
     </div>
   );
---
+++ src/components/Partners.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Partners = () => {
+  return (
+    <div className='flex justify-evenly m-4'>
+      <img className='' src='/assets/salesforc-gray.png'></img>
+      <img alt='groupon-logo' src='/assets/groupon-gray.png'></img>
+      <img alt='cisco-logo' src='/assets/cisco-gray.png'></img>
+      <img alt='wallgreens-logo' src='/assets/wallgreens-gray.png'></img>
+      <img alt='nasa-logo' className='pt-2' src='/assets/nasa-gray.png'></img>
+      <img alt='pwc-logo' src='/assets/pwc-gray.png'></img>
+    </div>
+  );
+};
+
+export default Partners;
